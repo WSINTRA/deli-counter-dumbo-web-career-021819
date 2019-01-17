f045f5c5158dffc70c56d512d8d54543049af575
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 21:34:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+  puts ("The line is currently: #{katz_deli}")
+end
+
+def take_a_number(katz_deli,line_of_ppl)
+  puts ("Welcome, #{katz_deli} You are number #{line_of_ppl+1} in line.")
+end
