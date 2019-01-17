2169b834da49b49b2a1c5457a356a6af7de0e69f
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 22:12:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,9 @@ end
 end
 
 def take_a_number(katz_deli,name)
+  katz_deli.push(name)
   line_number = katz_deli.length + 1
+
   puts ("Welcome, #{name}. You are number #{line_number} in line.")
 
 end
