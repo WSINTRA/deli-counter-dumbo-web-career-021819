09c5c5d844040537625ca2f2832cc4c406630de9
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 22:03:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ def line(katz_deli)
     puts ("The line is currently empty.")
   else
     katz_deli.each{|x| line.push(katz_deli.index(x)+1,",", x)}
+    line = line.join(', ')
   puts ("The line is currently: #{line}")
 end
 end
